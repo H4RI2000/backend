0200d41ccf106605f189bc98441be4ab9b712fb2
H4RI2000 <hari2000atl@gmail.com>
2025-09-21 13:23:57 +0530

    Updated backend code

--- hosproject/deployment_settings.py
+++ hosproject/deployment_settings.py
@@ -3,8 +3,8 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.get['hostname']]
-CSRF_TRUSTED_ORIGINS = ['http://'+os.environ.get['hostname']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['http://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
 
 
 DEBUG =False
