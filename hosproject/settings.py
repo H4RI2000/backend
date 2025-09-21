575629956cb51ffbe4e42b846c89b89a3ab1a595
H4RI2000 <hari2000atl@gmail.com>
2025-09-21 13:34:33 +0530

    Update ALLOWED_HOSTS for Render deployment

--- hosproject/settings.py
+++ hosproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0uio059#35m+iz&vk($g$0u$x$v$jm)s2%=7mdvtjdfk(fy!#r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
